6911451106a60d934bbc1161085568cf91b2b56b
sso05098 <sso05098@naver.com>
2024-05-10 15:21:39 +0900

    으에엥

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -4,12 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git World~~~");
-<<<<<<< HEAD
-		System.out.println("으에엥에ㄹㅀ에에");
-=======
-		System.out.println("kimojji");
->>>>>>> branch 'master' of https://github.com/sso05098/gitTest.git
-		
+
 	}
 
 }
