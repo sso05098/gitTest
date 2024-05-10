70e8e14d2908718b1cca940677de48429990e43f
sso05098 <sso05098@naver.com>
2024-05-10 15:14:12 +0900

    아에이오우

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git World~~~");
-		System.out.println("과제 안끝남");
+		System.out.println("으에엥에에에");
 		
 	}
 
